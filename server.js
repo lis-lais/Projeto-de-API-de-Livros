60613415c7c286a26c6fb44a547865ffbbadb985
Lais Lima <lis87lala@gmail.com>
2025-09-06 11:58:58 -0300

    Atualizando server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const app = require('./src/app');
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Conectado ao MongoDB');
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Servidor rodando em http://localhost:${PORT}`);
+    const PORT = process.env.PORT || 3000; // fallback para rodar local
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
     });
   })
   .catch((error) => {
